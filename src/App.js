cc8a2aea3cbc84757880978e7df1def54e188541
Arpit-Mall <arpitmall02@gmail.com>
2023-07-11 23:52:19 +0530

    Dropdown_List completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
+import Dropdown from "./components/Dropdown";
 import './App.css';
 
 function App() {
+
+  // These items would be displayed in the dropdown menu
+  const options = ["Yes", "No", "Probably Not"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      
+      {/* Question  */}
+      <h1>Q. Should you use a dropdown ?</h1>
+
+      <Dropdown options={options} />
     </div>
+
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+
+        <div className="header">
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/2976/2976215.png"
+                alt="bar- icon"
+
+            />
+            <h1>Dropdown Skill Test</h1>
+            <hr />
+        </div>
+
+    )
+}
+
+export default Header
